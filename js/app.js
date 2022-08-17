eebd24b15d11d6fe1081f4c19accc4cd1a87e9e1
Marc D Wagner <marc.wagner@yahoo.com>
2022-08-16 17:03:55 -0700

    started button and js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
+let name = prompt('What is your name?');
+console.log(name);
+alert('Welcome:' + ' name ');
+
 console.log('app is running');
+
+let takeTheQuiz = prompt('Do I live in Arizona?');
+console.log(); 
+
