3bb742476009a3040f68a54b996f1b5194f3cad0
Marc D Wagner <marc.wagner@yahoo.com>
2022-08-18 11:50:53 -0700

    more js won lab 3 section 3

--- js/app.js
+++ js/app.js
@@ -6,123 +6,125 @@ console.log('app is running');
 // let correctAnswers = 0;
 
 // User name
-let user = prompt('What is your name?');
-console.log(user);
-alert('Welcome: ' + user );
-if (user === null) {
-  alert('You need to provide your name.');
-  user = prompt('No, really what is your name?');
-}
+// let user = prompt('What is your name?');
+// console.log(user);
+// alert('Welcome: ' + user );
+// if (user === null) {
+//   alert('You need to provide your name.');
+//   user = prompt('No, really what is your name?');
+// }
 
 // Best military branch question
-let bestBranch = prompt('The Army is the best branch, right?');
-bestBranch = bestBranch.toLowerCase();
+// let bestBranch = prompt('The Army is the best branch, right?');
+// bestBranch = bestBranch.toLowerCase();
 
-switch (bestBranch) {
-case 'yes':
-  alert('You got that right!');
-  break;
-case 'no':
-  alert('At least you tried.');
-  break;
-default:
-  alert('Let me help, of course it is!');
-}
+// switch (bestBranch) {
+// case 'yes':
+//   alert('You got that right!');
+//   break;
+// case 'no':
+//   alert('At least you tried.');
+//   break;
+// default:
+//   alert('Let me help, of course it is!');
+// }
 
 // Favorite actor question
-let amazingActor = prompt('Was Heath Ledger\'s Joker one of the best acting performances ever?');
-amazingActor = amazingActor.toLowerCase();
+// let amazingActor = prompt('Was Heath Ledger\'s Joker one of the best acting performances ever?');
+// amazingActor = amazingActor.toLowerCase();
 
-switch (amazingActor) {
-case 'yes':
-  alert('Best ever!');
-  break;
-case 'no':
-  alert('You shouldn\'t be a critic.');
-  break;
-default:
-  alert('E for effort.');
-}
+// switch (amazingActor) {
+// case 'yes':
+//   alert('Best ever!');
+//   break;
+// case 'no':
+//   alert('You shouldn\'t be a critic.');
+//   break;
+// default:
+//   alert('E for effort.');
+// }
 
 // Travel location question
-let travelCroatia = prompt('Have you ever been to Croatia?');
-travelCroatia = travelCroatia.toLowerCase();
+// let travelCroatia = prompt('Have you ever been to Croatia?');
+// travelCroatia = travelCroatia.toLowerCase();
 
-switch (travelCroatia) {
-case 'yes':
-  alert('Then you know how beautiful it is too.');
-  break;
-case 'no':
-  alert('I highly recommend it, the water is beautiful.');
-  break;
-default:
-  alert('You should go sometime.');
-}
+// switch (travelCroatia) {
+// case 'yes':
+//   alert('Then you know how beautiful it is too.');
+//   break;
+// case 'no':
+//   alert('I highly recommend it, the water is beautiful.');
+//   break;
+// default:
+//   alert('You should go sometime.');
+// }
 
 // Arizona lightening question
-let arizonaLightening = prompt('Does Arizona have the best lightening storms?');
-arizonaLightening = arizonaLightening.toLowerCase();
+// let arizonaLightening = prompt('Does Arizona have the best lightening storms?');
+// arizonaLightening = arizonaLightening.toLowerCase();
 
-switch (arizonaLightening) {
-case 'yes':
-  alert('No contest.');
-  break;
-case 'no':
-  alert('Clearly don\'t know what you\'re talking about.');
-  break;
-default:
-  alert('Come on now.');
-}
+// switch (arizonaLightening) {
+// case 'yes':
+//   alert('No contest.');
+//   break;
+// case 'no':
+//   alert('Clearly don\'t know what you\'re talking about.');
+//   break;
+// default:
+//   alert('Come on now.');
+// }
 
 // Best pet question
-let bestPet = prompt('Bunnies are the best pet.');
-bestPet = bestPet.toLowerCase();
+// let bestPet = prompt('Bunnies are the best pet.');
+// bestPet = bestPet.toLowerCase();
 
-switch (bestPet) {
-case 'yes':
-  alert('You know it, and I know it.');
-  break;
-case 'no':
-  alert('Wasn\'t really asking.');
-  break;
-default:
-  alert('Wasn\'t really asking.');
-}
+// switch (bestPet) {
+// case 'yes':
+//   alert('You know it, and I know it.');
+//   break;
+// case 'no':
+//   alert('Wasn\'t really asking.');
+//   break;
+// default:
+//   alert('Wasn\'t really asking.');
+// }
 
 // Guess my favorite number loop
-let guessNumber = prompt('What is my favorite number, 1 - 50?');
-let favoriteNumber = '22';
+// let guessNumber = prompt('What is my favorite number, 1 - 50?');
+// let favoriteNumber = '22';
 
-for (let i = 1; i < 4; i++) {
-  if (guessNumber < favoriteNumber) {
-    alert('Your guess is too low.');
-  } else if (guessNumber > favoriteNumber) {
-    alert('Your guess is too high.');
-  } else {
-    alert('You got it!');
-    break;
-  }
-  guessNumber = prompt('What is my favorite number, 1 - 50');
-}
-if (guessNumber !== favoriteNumber) {
-  alert('You guessed too many times. The answer is 22.');
-}
+// for (let i = 1; i < 4; i++) {
+//   if (guessNumber < favoriteNumber) {
+//     alert('Your guess is too low.');
+//   } else if (guessNumber > favoriteNumber) {
+//     alert('Your guess is too high.');
+//   } else {
+//     alert('You got it!');
+//     break;
+//   }
+//   guessNumber = prompt('What is my favorite number, 1 - 50');
+// }
+// if (guessNumber !== favoriteNumber) {
+//   alert('You guessed too many times. The answer is 22.');
+// }
 
 // Guess states I've lived in loop
+let numberOfGuesses = 6;
 let statesLivedIn = prompt('Guess a state that I have lived in previously.');
 let answersStatesLivedIn = ['alaska', 'north carolina', 'georgia', 'new york', 'maryland', 'south carolina'];
-answersStatesLivedIn = statesLivedIn.toLowerCase();
 
-for (let i = 0; i < 6; i++) {
-  if (statesLivedIn === answersStatesLivedIn) {
-    alert('That is correct!  All states are Alaska, North Carolina, South Carolina, Georgia, New York and Maryland.');
-    break;
-  } else {
-    alert('That is not a state I have lived in.');
-  }
-  statesLivedIn = prompt('Guess a state that I have lived in previously.');
-  if (statesLivedIn !== answersStatesLivedIn) {
-    alert('You have exceeded your guesses, I have previously lived in the following states: Alaska, North Carolina, South Carolina, Georgia, New York and Maryland.');
+while(numberOfGuesses > 0) {
+  for (let i = 0; i < answersStatesLivedIn.length; i++) {
+    if (statesLivedIn.toLowerCase() === answersStatesLivedIn.toLowerCase()) {
+      alert('That is correct!  All states are Alaska, North Carolina, South Carolina, Georgia, New York and Maryland.');
+      break;
+    } else {
+      alert('That is not a state I have lived in.');
+    }
+    numberOfGuesses --;
+    statesLivedIn = prompt('Guess a state that I have lived in previously.');
+    if (numberOfGuesses === 0) {
+      alert('You have exceeded your guesses, I have previously lived in the following states: Alaska, North Carolina, South Carolina, Georgia, New York and Maryland.');
+    }
   }
 }
-
